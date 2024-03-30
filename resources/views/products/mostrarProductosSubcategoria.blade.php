659cd4f13128f0b46d0fbf1818b522c532242217
Cu3nz <sergiogallegit@gmail.com>
2024-03-30 18:02:44 +0100

    Añadido el id del producto para redireccionar al overview del producto

--- resources/views/products/mostrarProductosSubcategoria.blade.php
+++ resources/views/products/mostrarProductosSubcategoria.blade.php
@@ -1,8 +1,8 @@
 <x-app-layout>
     <x-propio>
-
         <section class=" gap-3  md:flex ">
             @foreach ($productos as $item)
+            <a href="">
                 <article>
                     <div
                         class="max-w-sm duration-150 hover:scale-105  bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
@@ -11,26 +11,27 @@
                                 alt="{{ $item->images->first()->desc_imagen }}" />
                         </a>
                         <div class="p-5">
-                            <a href="#">
                                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                                     {{ $item->nombre }}</h5>
-                            </a>
                             <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $item->descripcion }}</p>
                             <span class="text-red-600 inline-block">Precio: {{ $item->precio }}</span> <br><br>
                             <span class="text-red-600 mb-5 block">Stock: {{ $item->stock }}</span>
 
-                            <a href="#"
-                                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                                Read more
-                                <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
-                                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                        stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                            <a class="group  relative inline-flex items-center overflow-hidden rounded bg-indigo-600 px-8 py-3 text-white focus:outline-none focus:ring active:bg-indigo-500"
+                            href="{{route('overviewProduct' , $item -> id)}}"> {{-- ? Le paso el id del producto al cual he hecho click --}}
+                            <span class="absolute -end-full transition-all group-hover:end-4">
+                                <svg class="size-5 rtl:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                    viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                        d="M17 8l4 4m0 0l-4 4m4-4H3" />
                                 </svg>
-                            </a>
+                            </span>
+                            <span class="text-sm font-medium transition-all group-hover:me-4">Ver más</span>
+                        </a>
                         </div>
                     </div>
                 </article>
+            </a>
             @endforeach
         </section>
 
