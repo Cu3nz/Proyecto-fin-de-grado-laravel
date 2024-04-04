1f88e1f2c3fa66d3f19495e63339a3f62b063a6a
Cu3nz <sergiogallegit@gmail.com>
2024-04-04 23:39:40 +0200

    Añadida la miga de pan Home

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,4 +1,6 @@
-<x-app-layout>    
+<x-app-layout>
+    {{-- ? Miga de pan 1 --}}
+    {{ Breadcrumbs::render('dashboard') }}      
     <div class="bg-white">
         <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
             <h2 class="text-2xl font-bold tracking-tight  text-gray-900">Anime</h2>
