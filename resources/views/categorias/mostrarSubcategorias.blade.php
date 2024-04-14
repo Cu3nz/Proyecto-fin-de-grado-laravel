c6db5f90d4d289c4658f2ba23902e0e4926d4ea1
Cu3nz <sergiogallegit@gmail.com>
2024-04-14 21:17:52 +0200

    Cards mejoradas y responsivas

--- resources/views/categorias/mostrarSubcategorias.blade.php
+++ resources/views/categorias/mostrarSubcategorias.blade.php
@@ -1,35 +1,32 @@
 <x-app-layout>
-    <div class="container mx-auto px-4">
+    {{-- todo Sergio 14 abril --}}
+    <div class="container mx-auto mb-2 px-4">
         {{-- ? Miga de pan 6 --}}
         {{ Breadcrumbs::render('category.subcategorias', $subcategoriasDePadre) }}
         <h2 class="text-xl font-bold mb-4">Subcategorías de {{ $subcategoriasDePadre->nombre }}</h2>
-        <section class="grid  grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        <section class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
             @foreach ($subcategoriasDePadre->children as $subcategoria)
-                <article
-                    class="max-w-sm duration-150 hover:scale-105 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                    <a href="#">
-                        <img class="rounded-t-lg object-cover object-center" src="{{ Storage::url($subcategoria->image->url_imagen) }}"
-                            alt="{{ $subcategoria->image->desc_imagen }}" />
+                <article class="flex flex-col duration-150 hover:scale-105 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden">
+                    <a href="{{ route('productosConSubcategoria', $subcategoria->id) }}" class="block">
+                        <img class="w-full h-50 lg:h-48 object-cover" src="{{ Storage::url($subcategoria->image->url_imagen) }}" alt="{{ $subcategoria->image->desc_imagen }}" />
                     </a>
-                    <div class="p-5">
-                            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    <div class="p-5 flex flex-col justify-between h-full">
+                        <div>
+                            <h5 class="text-xl lg:text-2xl font-bold tracking-tight text-gray-900 dark:text-white mb-2">
                                 {{ $subcategoria->nombre }}</h5>
-                        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ $subcategoria->descripcion}}
-                        </p>
-                        <span class="text-red-600 inline-block">Category_padre_id:{{$subcategoria -> category_padre_id}}</span> <br><br>
-                        <span class="text-red-600 mb-5 block">ID categoria{{$subcategoria -> id}}</span> 
-                        
-
-                        <a class="group  relative inline-flex items-center overflow-hidden rounded bg-indigo-600 px-8 py-3 text-white focus:outline-none focus:ring active:bg-indigo-500"
-                            href="{{route('productosConSubcategoria' , $subcategoria ->  id)}}"> {{-- ? le pasamos la id padre de esa subcategoria , para que coja todos los productos en la tabla products donde el atributo category_id tenga el mismo id que se pasa por esa ruta --}}
-                            <span class="absolute -end-full transition-all group-hover:end-4">
-                                <svg class="size-5 rtl:rotate-180" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                    viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M17 8l4 4m0 0l-4 4m4-4H3" />
-                                </svg>
-                            </span>
-                            <span class="text-sm font-medium transition-all group-hover:me-4">Ver más</span>
+                                <div class="overflow-y-auto scrollbar-hidden-y max-h-24 mb-2">
+                                    <p class="text-sm text-gray-700 dark:text-gray-400">{{ $subcategoria->descripcion }}</p>
+                                </div>
+                        </div>
+                        <div>
+                            <span class="text-red-600">Category_padre_id:{{ $subcategoria->category_padre_id }}</span>
+                            <span class="text-red-600 my-2">ID categoria:{{ $subcategoria->id }}</span>
+                        </div>
+                        <a class="mt-auto group relative flex w-full justify-center items-center overflow-hidden rounded bg-indigo-600 py-3 text-white focus:outline-none focus:ring active:bg-indigo-500" href="{{route('productosConSubcategoria', $subcategoria->id)}}">
+                            <span class="text-sm font-medium transition-all group-hover:translate-x-2">Ver más</span>
+                            <svg class="h-5 w-5 transition-all opacity-0 group-hover:opacity-100 absolute right-16 mr-3 md:mr-0  transform group-hover:translate-x-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" style="transition-delay: 75ms;">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+                            </svg>
                         </a>
                     </div>
                 </article>
