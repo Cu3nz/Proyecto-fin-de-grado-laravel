bdff9342066c01450d3890ec521d8933a84e685b
Cu3nz <sergiogallegit@gmail.com>
2024-05-18 06:41:59 +0200

    Añadida el import de las nuevas alertas

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
 import './bootstrap';
+import { Notyf } from 'notyf/notyf.es.js';
+window.notyf = new Notyf(); // Asigna a window para hacerlo global
+// Exporta notyf para uso global si es necesario
+export default notyf;
