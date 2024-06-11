543271a5063e62bfc471fbed5cca769c229c6551
Cu3nz <sergiogallegit@gmail.com>
2024-06-11 13:09:06 +0200

    Vista que hace el boton para añadir al carrito, luego se pone en overProduct con un livewire y el nombre de la vista

---
+++ resources/views/livewire/btn-añadir-carrito.blade.php
@@ -0,0 +1,3 @@
+<button wire:click="addProduct" class="flex max-w-xs flex-1 items-center justify-center rounded-md border border-transparent rosa transition duration-200 ease-in px-8 py-3 text-base  text-white font-bold  sm:w-full">
+    Añadir al carrito
+</button>
