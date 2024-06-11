d4f034f2021fa3ee6cf2d4ed03c904479b71682c
Cu3nz <sergiogallegit@gmail.com>
2024-06-11 22:36:35 +0200

    Cambiado de grid a flex

--- resources/views/categorias/mostrarCategoriasPadres.blade.php
+++ resources/views/categorias/mostrarCategoriasPadres.blade.php
@@ -1,44 +1,29 @@
 <x-app-layout>
-  {{-- ? Miga de pan 5 --}}
-  {{ Breadcrumbs::render('category.index') }}
-    <x-propio>
-        <section class="flex flex-col items-center md:flex-row md:justify-center md:flex-wrap">
-            @foreach ($categoriasPadres as $item)
-                <article class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 p-4">
-                    <a href="{{ route('category.subcategorias', $item->id) }}"> {{-- ? Le pasamos la id de la categoria padre, para coger las subcategorias de esa categoria padre --}}
-                        <div class="bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-                            <img class="p-8 rounded-t-lg" src="{{ Storage::url($item->image->url_imagen) }}"
-                                alt="{{$item -> image -> desc_imagen}}" />
-                            <div class="px-5 pb-5">
-                                <h5
-                                    class="text-xl text-center font-semibold tracking-tight text-gray-900 dark:text-white">
-                                    {{ $item->nombre }}</h5>
-                                <div class="flex justify-center items-center mt-2.5 mb-5">
-                                    <button
-                                    class="overflow-hidden w-32 p-2 h-12 bg-black text-white border-none rounded-md text-xl font-bold cursor-pointer relative z-10 group"
-                                  >
-                                    Hover me!
-                                    <span
-                                      class="absolute w-36 h-32 -top-8 -left-2 bg-green-200 rounded-full transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-500 duration-700 origin-bottom"
-                                    ></span>
-                                    <span
-                                      class="absolute w-36 h-32 -top-8 -left-2 bg-green-400 rounded-full transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-700 duration-500 origin-bottom"
-                                    ></span>
-                                    <span
-                                      class="absolute w-36 h-32 -top-8 -left-2 bg-green-600 rounded-full transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-1000 duration-200 origin-bottom"
-                                    ></span>
-                                    <span
-                                      class="group-hover:opacity-100 group-hover:duration-1000 duration-100 opacity-0 absolute top-2.5 left-6 z-10"
-                                      >Explore!</span
-                                    >
-                                  </button>
-                                  
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </article>
-            @endforeach
-        </section>
-    </x-propio>
+  {{-- Miga de pan --}}
+  <x-propio>
+      {{ Breadcrumbs::render('category.index') }}
+      <section class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 sm:gap-4 px-4 py-5">
+          @foreach ($categoriasPadres as $item)
+              <article class="transform transition-transform duration-300 ease-in-out hover:scale-105">
+                  <a href="{{ route('category.subcategorias', $item->id) }}" class="block">
+                      <div class="bg-white border border-gray-200 rounded-lg shadow-md hover:shadow-lg">
+                          <img class="p-8 w-full h-96 object-cover rounded-t-lg" src="{{ Storage::url($item->image->url_imagen) }}" alt="{{ $item->image->desc_imagen }}" />
+                          <div class="px-5 py-4">
+                              <h5 class="text-xl font-semibold tracking-tight text-gray-900 text-center">{{ $item->nombre }}</h5>
+                              <p class="mt-2 text-gray-700 text-center h-24 overflow-y-auto">{{ $item->descripcion }}</p>
+                              <div class="flex justify-center mt-4">
+                                  <a class="mt-auto group relative flex w-full justify-center items-center overflow-hidden rounded rosa transition duration-200 ease-in py-3 text-white focus:outline-none focus:ring active:bg-indigo-500" href="{{ route('category.subcategorias', $item->id) }}">
+                                      <span class="text-sm font-medium transition-all group-hover:translate-x-2">Ver más</span>
+                                      <svg class="h-5 w-5 transition-all opacity-0 group-hover:opacity-100 absolute right-16 mr-10 md:mr-10 transform group-hover:translate-x-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" style="transition-delay: 75ms;">
+                                          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 8l4 4m0 0l-4 4m4-4H3" />
+                                      </svg>
+                                  </a>
+                              </div>
+                          </div>
+                      </div>
+                  </a>
+              </article>
+          @endforeach
+      </section>
+  </x-propio>
 </x-app-layout>
