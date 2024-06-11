a009f9b45e25ccb0449efa5e80d404d4371b1531
Cu3nz <sergiogallegit@gmail.com>
2024-06-11 23:10:35 +0200

    Politica para prohibir el borrado de productos que no son tuyos

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\ShoppingCart;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        //todo Defino la politica que hace que un usuario no borre productos de otro usuario, SOLO PUEDE BORRAR SUS PRODUCTOS
+         //? Para que no se pueda eliminar un producto que no sea tuyo. 
+         Gate::define('removeProductCarrito' , function(User $user , ShoppingCart $product){
+            //* Dejara borrar el producto si el user -> id que se ha registrado es igual al user_id del producto
+            return $user->id === $product->user_id;
+        });
     }
 }
